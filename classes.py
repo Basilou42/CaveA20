b9f1e95797b35534cffb673b2ee4d95f958787d1
Basilou42 <basile.stel@gmail.com>
2024-10-15 14:38:30 +0200

    unittest => pytest fixed a lot of errors the database is implemented in the app now

--- classes.py
+++ classes.py
@@ -3,161 +3,179 @@ import hashlib
 
 # Connexion à la base de données
 def get_db_connection():
-	return mysql.connector.connect(
-		host="localhost",   # Remplace par ton hôte MariaDB
-		user="root",        # Remplace par ton utilisateur MariaDB
-		password="mysqlroot",
-		port=3306,# Remplace par ton mot de passe
-		database="gestion_vins"
-	)
+    return mysql.connector.connect(
+        host="localhost",   # Remplace par ton hôte MariaDB
+        user="root",        # Remplace par ton utilisateur MariaDB
+        password="mysqlroot",
+        port=3306,# Remplace par ton mot de passe
+        database="gestion_vins"
+    )
 
 class User:
-	def __init__(self, username, password):
-		self.username = username
-		self.password_hash = self.hash_password(password)
+    def __init__(self, username, password):
+        self.username = username
+        self.password_hash = self.hash_password(password)
 
-	def hash_password(self, password):
-		return hashlib.sha256(password.encode()).hexdigest()
+    def hash_password(self, password):
+        return hashlib.sha256(password.encode()).hexdigest()
 
-	def verify_password(self, password):
-		return self.password_hash == hashlib.sha256(password.encode()).hexdigest()
+    def verify_password(self, password):
+        return self.password_hash == hashlib.sha256(password.encode()).hexdigest()
 
-	def save(self):
-		db = get_db_connection()
-		cursor = db.cursor()
+    def save(self):
+        db = get_db_connection()
+        cursor = db.cursor()
 
-		# Insertion de l'utilisateur dans la base de données
-		cursor.execute("INSERT INTO users (username, password_hash) VALUES (%s, %s)", 
-					   (self.username, self.password_hash))
-		db.commit()
+        # Check if the username already exists
+        cursor.execute("SELECT COUNT(*) FROM users WHERE username = %s", (self.username,))
+        if cursor.fetchone()[0] > 0:
+            raise Exception(f"Username '{self.username}' already exists.")
+        
+        cursor.execute("INSERT INTO users (username, password_hash) VALUES (%s, %s)", 
+                       (self.username, self.password_hash))
+        db.commit()
 
-		# Récupération du user_id généré par la base de données
-		self.user_id = cursor.lastrowid
+        self.user_id = cursor.lastrowid
+        cursor.close()
+        db.close()
 
-		cursor.close()
-		db.close()
 
-	@staticmethod
-	def get_by_username(username):
-		db = get_db_connection()
-		cursor = db.cursor(dictionary=True)
-		cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
-		user_data = cursor.fetchone()
-		cursor.close()
-		db.close()
-		if user_data:
-			user = User(user_data['username'], user_data['password_hash'])
-			user.user_id = user_data['user_id']
-			return user
-		return None
+    @staticmethod
+    def get_by_username(username):
+        db = get_db_connection()
+        cursor = db.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+        user_data = cursor.fetchone()
+        cursor.close()
+        db.close()
+        if user_data:
+            # Create the User object without hashing the password
+            user = User(user_data['username'], '')  # Pass a placeholder password
+            user.password_hash = user_data['password_hash']  # Assign the password hash directly
+            user.user_id = user_data['user_id']
+            return user
+        return None
 
-	def ajouter_cave(self, nom):
-		db = get_db_connection()
-		cursor = db.cursor()
-		cursor.execute("INSERT INTO caves (nom, user_id) VALUES (%s, %s)", (nom, self.user_id))
-		db.commit()
-		cursor.close()
-		db.close()
+
+
+    def ajouter_cave(self, nom):
+        db = get_db_connection()
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO caves (nom, user_id) VALUES (%s, %s)", (nom, self.user_id))
+        db.commit()
+        cursor.close()
+        db.close()
 
 class Cave:
-	def __init__(self, nom, user_id):
-		self.nom = nom
-		self.user_id = user_id
+    def __init__(self, nom, user_id):
+        self.nom = nom
+        self.user_id = user_id
 
-	@staticmethod
-	def get_caves_by_user(user_id):
-		db = get_db_connection()
-		cursor = db.cursor(dictionary=True)
-		cursor.execute("SELECT * FROM caves WHERE user_id = %s", (user_id,))
-		caves = cursor.fetchall()
-		cursor.close()
-		db.close()
-		return caves
+    @staticmethod
+    def get_caves_by_user(user_id):
+        db = get_db_connection()
+        cursor = db.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM caves WHERE user_id = %s", (user_id,))
+        caves = cursor.fetchall()
+        cursor.close()
+        db.close()
+        return caves
 
-	def ajouter_etagere(self, emplacements):
-		db = get_db_connection()
-		cursor = db.cursor()
-		cursor.execute("INSERT INTO etageres (num_etagere, emplacements, cave_id) VALUES (%s, %s, %s)",
-					   (self.num_etagere, emplacements, self.cave_id))
-		db.commit()
-		cursor.close()
-		db.close()
+    def ajouter_etagere(self, emplacements):
+        db = get_db_connection()
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO etageres (num_etagere, emplacements, cave_id) VALUES (%s, %s, %s)",
+                       (self.num_etagere, emplacements, self.cave_id))
+        db.commit()
+        cursor.close()
+        db.close()
 
 class Etagere:
-	def __init__(self, num_etagere, emplacements, cave_id):
-		self.num_etagere = num_etagere
-		self.emplacements = emplacements
-		self.cave_id = cave_id
+    def __init__(self, num_etagere, emplacements, cave_id, etagere_id=None):
+        self.num_etagere = num_etagere
+        self.emplacements = emplacements
+        self.cave_id = cave_id
+        self.etagere_id = etagere_id
 
-	@staticmethod
-	def get_etageres_by_cave(cave_id):
-		db = get_db_connection()
-		cursor = db.cursor(dictionary=True)
-		cursor.execute("SELECT * FROM etageres WHERE cave_id = %s", (cave_id,))
-		etageres = cursor.fetchall()
-		cursor.close()
-		db.close()
-		return etageres
+    @staticmethod
+    def get_etageres_by_cave(cave_id):
+        db = get_db_connection()
+        cursor = db.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM etageres WHERE cave_id = %s", (cave_id,))
+        etageres = cursor.fetchall()  # Fetch all shelves associated with the given cave_id
+        cursor.close()
+        db.close()
+        return etageres
 
-	def ajouter_bouteille(self, bouteille):
-		db = get_db_connection()
-		cursor = db.cursor()
-		if self.place_libre(bouteille.quantite):
-			cursor.execute("INSERT INTO bouteilles (domaine, nom, type_vin, region, annee, prix, photo, quantite, etagere_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
-						   (bouteille.domaine, bouteille.nom, bouteille.typeVin, bouteille.region, bouteille.annee, bouteille.prix, bouteille.photo, bouteille.quantite, self.etagere_id))
-			db.commit()
-		else:
-			raise Exception("Pas assez de place dans l'étagère")
-		cursor.close()
-		db.close()
+    def ajouter_etagere(self, emplacements):
+        db = get_db_connection()
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO etageres (num_etagere, emplacements, cave_id) VALUES (%s, %s, %s)",
+                       (self.num_etagere, emplacements, self.cave_id))
+        db.commit()
+        self.etagere_id = cursor.lastrowid  # Set the etagere_id
+        cursor.close()
+        db.close()
 
-	def place_libre(self, nombre_bouteilles=1):
-		db = get_db_connection()
-		cursor = db.cursor()
-		cursor.execute("SELECT COUNT(*) FROM bouteilles WHERE etagere_id = %s", (self.etagere_id,))
-		nb_bouteilles = cursor.fetchone()[0]
-		cursor.close()
-		db.close()
-		return nb_bouteilles + nombre_bouteilles <= self.emplacements
+    def place_libre(self, nombre_bouteilles=1):
+        db = get_db_connection()
+        cursor = db.cursor()
+        cursor.execute("SELECT COUNT(*) FROM bouteilles WHERE etagere_id = %s", (self.etagere_id,))
+        nb_bouteilles = cursor.fetchone()[0]
+        cursor.close()
+        db.close()
+        return nb_bouteilles + nombre_bouteilles <= self.emplacements
+
+    def ajouter_bouteille(self, bouteille):
+        db = get_db_connection()
+        cursor = db.cursor()
+        if self.place_libre(bouteille.quantite):
+            cursor.execute("INSERT INTO bouteilles (domaine, nom, type_vin, region, annee, prix, photo, quantite, etagere_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                           (bouteille.domaine, bouteille.nom, bouteille.typeVin, bouteille.region, bouteille.annee, bouteille.prix, bouteille.photo, bouteille.quantite, self.etagere_id))
+            db.commit()
+        else:
+            raise Exception("Pas assez de place dans l'étagère")
+        cursor.close()
+        db.close()
 
 class Bouteille:
-	def __init__(self, domaine, nom, typeVin, region, annee, prix, photo, quantite=1):
-		self.domaine = domaine
-		self.nom = nom
-		self.typeVin = typeVin
-		self.region = region
-		self.annee = annee
-		self.prix = prix
-		self.photo = photo
-		self.quantite = quantite
+    def __init__(self, domaine, nom, typeVin, region, annee, prix, photo, quantite=1):
+        self.domaine = domaine
+        self.nom = nom
+        self.typeVin = typeVin
+        self.region = region
+        self.annee = annee
+        self.prix = prix
+        self.photo = photo
+        self.quantite = quantite
 
-	@staticmethod
-	def get_bouteilles_by_etagere(etagere_id):
-		db = get_db_connection()
-		cursor = db.cursor(dictionary=True)
-		cursor.execute("SELECT * FROM bouteilles WHERE etagere_id = %s", (etagere_id,))
-		bouteilles = cursor.fetchall()
-		cursor.close()
-		db.close()
-		return bouteilles
+    @staticmethod
+    def get_bouteilles_by_etagere(etagere_id):
+        db = get_db_connection()
+        cursor = db.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM bouteilles WHERE etagere_id = %s", (etagere_id,))
+        bouteilles = cursor.fetchall()
+        cursor.close()
+        db.close()
+        return bouteilles
 
 class Communaute:
-	@staticmethod
-	def ajouter_note_commentaire(user_id, bouteille_id, note, commentaire):
-		db = get_db_connection()
-		cursor = db.cursor()
-		cursor.execute("INSERT INTO notes_commentaires (user_id, bouteille_id, note, commentaire) VALUES (%s, %s, %s, %s)",
-					   (user_id, bouteille_id, note, commentaire))
-		db.commit()
-		cursor.close()
-		db.close()
+    @staticmethod
+    def ajouter_note_commentaire(user_id, bouteille_id, note, commentaire):
+        db = get_db_connection()
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO notes_commentaires (user_id, bouteille_id, note, commentaire) VALUES (%s, %s, %s, %s)",
+                       (user_id, bouteille_id, note, commentaire))
+        db.commit()
+        cursor.close()
+        db.close()
 
-	@staticmethod
-	def calculer_note_moyenne(bouteille_id):
-		db = get_db_connection()
-		cursor = db.cursor()
-		cursor.execute("SELECT AVG(note) FROM notes_commentaires WHERE bouteille_id = %s", (bouteille_id,))
-		moyenne = cursor.fetchone()[0]
-		cursor.close()
-		db.close()
-		return moyenne
+    @staticmethod
+    def calculer_note_moyenne(bouteille_id):
+        db = get_db_connection()
+        cursor = db.cursor()
+        cursor.execute("SELECT AVG(note) FROM notes_commentaires WHERE bouteille_id = %s", (bouteille_id,))
+        moyenne = cursor.fetchone()[0]
+        cursor.close()
+        db.close()
+        return moyenne
--- tests.py
+++ tests.py
@@ -1,161 +1,186 @@
-import unittest
 import mysql.connector
 from classes import User, Cave, Etagere, Bouteille, Communaute, get_db_connection
+import pytest
 
-class TestVinAppDB(unittest.TestCase):
+# Helper function to create users
+def create_users():
+    """Creates and returns two users."""
+    user1 = User("JohnDoe", "password123")
+    user1.save()
 
-	def setUp(self):
-		"""Initialisation de la connexion à la base de données et création d'utilisateurs"""
-		# Connexion à la base de données
-		self.db = get_db_connection()
-		self.cursor = self.db.cursor()
+    user2 = User("JaneDoe", "password456")
+    user2.save()
 
-		# Créer des utilisateurs pour les tests
-		self.user1 = User("JohnDoe", "password123")
-		self.user1.save()
+    user1 = User.get_by_username("JohnDoe")
+    user2 = User.get_by_username("JaneDoe")
+    return user1, user2
 
-		self.user2 = User("JaneDoe", "password456")
-		self.user2.save()
+# Helper function to create a bottle
+def create_bottle(name, appellation, couleur, region, annee, prix, photo, quantite=1):
+    """Creates and returns a bottle object."""
+    return Bouteille(name, appellation, couleur, region, annee, prix, photo, quantite)
 
-		# Récupérer les utilisateurs à partir de la base de données
-		self.user1 = User.get_by_username("JohnDoe")
-		self.user2 = User.get_by_username("JaneDoe")
+@pytest.fixture(scope="function")
+def setup_db():
+    """Initialisation de la connexion à la base de données et nettoyage"""
+    db = get_db_connection()
+    cursor = db.cursor()
 
-		# Créer une cave pour l'utilisateur 1
-		self.user1.ajouter_cave("Cave à vin John")
+    # Clean the database at the start of each test to avoid duplicates
+    cursor.execute("SET FOREIGN_KEY_CHECKS=0;")
+    cursor.execute("TRUNCATE TABLE notes_commentaires;")
+    cursor.execute("TRUNCATE TABLE bouteilles;")
+    cursor.execute("TRUNCATE TABLE etageres;")
+    cursor.execute("TRUNCATE TABLE caves;")
+    cursor.execute("TRUNCATE TABLE users;")
+    cursor.execute("SET FOREIGN_KEY_CHECKS=1;")
+    db.commit()
 
-		# Récupérer la cave créée pour ajouter des étagères
-		caves = Cave.get_caves_by_user(self.user1.user_id)
-		self.cave1 = caves[0]
+    # Créer des utilisateurs pour les tests
+    user1, user2 = create_users()
 
-		# Ajouter une étagère avec 5 emplacements
-		self.etagere1 = Etagere(1, 5, self.cave1['cave_id'])
-		self.etagere1.ajouter_etagere(5)
+    # Créer une cave pour l'utilisateur 1
+    user1.ajouter_cave("Cave à vin John")
 
+    # Récupérer la cave créée pour ajouter des étagères
+    caves = Cave.get_caves_by_user(user1.user_id)
+    cave1 = caves[0]
 
-	def tearDown(self):
-		"""Nettoyage de la base de données après chaque test"""
-		self.cursor.execute("SET FOREIGN_KEY_CHECKS=0;")
-		self.cursor.execute("TRUNCATE TABLE notes_commentaires;")
-		self.cursor.execute("TRUNCATE TABLE bouteilles;")
-		self.cursor.execute("TRUNCATE TABLE etageres;")
-		self.cursor.execute("TRUNCATE TABLE caves;")
-		self.cursor.execute("TRUNCATE TABLE users;")
-		self.cursor.execute("SET FOREIGN_KEY_CHECKS=1;")
-		self.db.commit()
+    # Ajouter une étagère avec 5 emplacements
+    etagere1 = Etagere(1, 5, cave1['cave_id'])
+    etagere1.ajouter_etagere(5)
 
-	def test_creation_utilisateur(self):
-		"""Test de la création des utilisateurs et de la récupération depuis la base de données"""
-		self.assertIsNotNone(self.user1)
-		self.assertIsNotNone(self.user2)
-		self.assertEqual(self.user1.username, "JohnDoe")
-		self.assertEqual(self.user2.username, "JaneDoe")
+    yield db, cursor, user1, user2, cave1, etagere1
 
-	def test_verification_mot_de_passe(self):
-		"""Test de la vérification du mot de passe"""
-		self.assertTrue(self.user1.verify_password("password123"))
-		self.assertFalse(self.user1.verify_password("wrongpassword"))
+    # Cleanup after each test
+    cursor.execute("SET FOREIGN_KEY_CHECKS=0;")
+    cursor.execute("TRUNCATE TABLE notes_commentaires;")
+    cursor.execute("TRUNCATE TABLE bouteilles;")
+    cursor.execute("TRUNCATE TABLE etageres;")
+    cursor.execute("TRUNCATE TABLE caves;")
+    cursor.execute("TRUNCATE TABLE users;")
+    cursor.execute("SET FOREIGN_KEY_CHECKS=1;")
+    db.commit()
 
-	def test_ajout_cave(self):
-		"""Test de l'ajout d'une cave pour un utilisateur"""
-		caves = Cave.get_caves_by_user(self.user1.user_id)
-		self.assertEqual(len(caves), 1)
-		self.assertEqual(caves[0]['nom'], "Cave à vin John")
+def test_creation_utilisateur(setup_db):
+    """Test de la création des utilisateurs et de la récupération depuis la base de données"""
+    _, _, user1, user2, _, _ = setup_db
+    assert user1 is not None
+    assert user2 is not None
+    assert user1.username == "JohnDoe"
+    assert user2.username == "JaneDoe"
 
-	def test_ajout_etagere(self):
-		"""Test de l'ajout d'une étagère dans la cave"""
-		etageres = Etagere.get_etageres_by_cave(self.cave1['cave_id'])
-		self.assertEqual(len(etageres), 1)
-		self.assertEqual(etageres[0]['num_etagere'], 1)
+def test_verification_mot_de_passe(setup_db):
+    """Test de la vérification du mot de passe"""
+    _, _, user1, _, _, _ = setup_db
+    assert user1.verify_password("password123")
+    assert not user1.verify_password("wrongpassword")
 
-	def test_ajout_bouteille(self):
-		"""Test de l'ajout d'une bouteille dans une étagère"""
-		bouteille = Bouteille("Domaine de la Romanée-Conti", "Romanée-Conti", "Rouge", "Bourgogne", 2015, 15000, "photo.jpg")
-		self.etagere1.ajouter_bouteille(bouteille)
+def test_ajout_cave(setup_db):
+    """Test de l'ajout d'une cave pour un utilisateur"""
+    _, _, user1, _, cave1, _ = setup_db
+    caves = Cave.get_caves_by_user(user1.user_id)
+    assert len(caves) == 1
+    assert caves[0]['nom'] == "Cave à vin John"
 
-		bouteilles = Bouteille.get_bouteilles_by_etagere(self.etagere1.etagere_id)
-		self.assertEqual(len(bouteilles), 1)
-		self.assertEqual(bouteilles[0]['nom'], "Romanée-Conti")
+def test_ajout_etagere(setup_db):
+    """Test de l'ajout d'une étagère dans la cave"""
+    _, _, _, _, cave1, _ = setup_db
+    etageres = Etagere.get_etageres_by_cave(cave1['cave_id'])
+    assert len(etageres) == 1
+    assert etageres[0]['num_etagere'] == 1
 
-	def test_retrait_bouteille(self):
-		"""Test du retrait d'une bouteille d'une étagère"""
-		bouteille = Bouteille("Domaine de la Romanée-Conti", "Romanée-Conti", "Rouge", "Bourgogne", 2015, 15000, "photo.jpg")
-		self.etagere1.ajouter_bouteille(bouteille)
+def test_ajout_bouteille(setup_db):
+    """Test de l'ajout d'une bouteille dans une étagère"""
+    db, cursor, _, _, _, etagere1 = setup_db
+    bouteille = create_bottle("Domaine de la Romanée-Conti", "Romanée-Conti", "Rouge", "Bourgogne", 2015, 15000, "photo.jpg")
+    etagere1.ajouter_bouteille(bouteille)
 
-		# Vérifier que la bouteille a été ajoutée
-		bouteilles = Bouteille.get_bouteilles_by_etagere(self.etagere1.etagere_id)
-		self.assertEqual(len(bouteilles), 1)
+    bouteilles = Bouteille.get_bouteilles_by_etagere(etagere1.etagere_id)
+    assert len(bouteilles) == 1
+    assert bouteilles[0]['nom'] == "Romanée-Conti"
 
-		# Supprimer la bouteille
-		self.cursor.execute("DELETE FROM bouteilles WHERE nom = 'Romanée-Conti'")
-		self.db.commit()
+def test_retrait_bouteille(setup_db):
+    """Test du retrait d'une bouteille d'une étagère"""
+    db, cursor, _, _, _, etagere1 = setup_db
+    bouteille = create_bottle("Domaine de la Romanée-Conti", "Romanée-Conti", "Rouge", "Bourgogne", 2015, 15000, "photo.jpg")
+    etagere1.ajouter_bouteille(bouteille)
 
-		# Vérifier que la bouteille a été supprimée
-		bouteilles = Bouteille.get_bouteilles_by_etagere(self.etagere1.etagere_id)
-		self.assertEqual(len(bouteilles), 0)
+    bouteilles = Bouteille.get_bouteilles_by_etagere(etagere1.etagere_id)
+    assert len(bouteilles) == 1
 
-	def test_ajout_lots_bouteilles(self):
-		"""Test de l'ajout de plusieurs bouteilles identiques en lot"""
-		bouteille_lot = Bouteille("Château Margaux", "Margaux", "Rouge", "Bordeaux", 2016, 1200, "photo.jpg", quantite=10)
+    cursor.execute("DELETE FROM bouteilles WHERE nom = 'Romanée-Conti'")
+    db.commit()
 
-		with self.assertRaises(Exception):
-			self.etagere1.ajouter_bouteille(bouteille_lot)
+    bouteilles = Bouteille.get_bouteilles_by_etagere(etagere1.etagere_id)
+    assert len(bouteilles) == 0
 
-	def test_lister_bouteilles(self):
-		"""Test de la liste des bouteilles dans toutes les caves"""
-		bouteille1 = Bouteille("Domaine de la Romanée-Conti", "Romanée-Conti", "Rouge", "Bourgogne", 2015, 15000, "photo.jpg")
-		bouteille2 = Bouteille("Château Margaux", "Margaux", "Rouge", "Bordeaux", 2016, 1200, "photo.jpg")
+def test_ajout_lots_bouteilles(setup_db):
+    """Test de l'ajout de plusieurs bouteilles identiques en lot"""
+    _, _, _, _, _, etagere1 = setup_db
+    bouteille_lot = create_bottle("Château Margaux", "Margaux", "Rouge", "Bordeaux", 2016, 1200, "photo.jpg", quantite=10)
 
-		self.etagere1.ajouter_bouteille(bouteille1)
-		self.etagere1.ajouter_bouteille(bouteille2)
+    with pytest.raises(Exception):
+        etagere1.ajouter_bouteille(bouteille_lot)
 
-		bouteilles = Bouteille.get_bouteilles_by_etagere(self.etagere1.etagere_id)
-		self.assertEqual(len(bouteilles), 2)
+def test_lister_bouteilles(setup_db):
+    """Test de la liste des bouteilles dans toutes les caves"""
+    _, _, _, _, _, etagere1 = setup_db
+    bouteille1 = create_bottle("Domaine de la Romanée-Conti", "Romanée-Conti", "Rouge", "Bourgogne", 2015, 15000, "photo.jpg")
+    bouteille2 = create_bottle("Château Margaux", "Margaux", "Rouge", "Bordeaux", 2016, 1200, "photo.jpg")
 
-	def test_tri_bouteilles(self):
-		"""Test du tri des bouteilles par année"""
-		bouteille1 = Bouteille("Domaine de la Romanée-Conti", "Romanée-Conti", "Rouge", "Bourgogne", 2015, 15000, "photo.jpg")
-		bouteille2 = Bouteille("Château Margaux", "Margaux", "Rouge", "Bordeaux", 2016, 1200, "photo.jpg")
+    etagere1.ajouter_bouteille(bouteille1)
+    etagere1.ajouter_bouteille(bouteille2)
 
-		self.etagere1.ajouter_bouteille(bouteille1)
-		self.etagere1.ajouter_bouteille(bouteille2)
+    bouteilles = Bouteille.get_bouteilles_by_etagere(etagere1.etagere_id)
+    assert len(bouteilles) == 2
 
-		self.cursor.execute("SELECT * FROM bouteilles ORDER BY annee")
-		bouteilles_triees = self.cursor.fetchall()
+def test_tri_bouteilles(setup_db):
+    """Test du tri des bouteilles par année"""
+    db, cursor, _, _, _, etagere1 = setup_db
+    bouteille1 = create_bottle("Domaine de la Romanée-Conti", "Romanée-Conti", "Rouge", "Bourgogne", 2015, 15000, "photo.jpg")
+    bouteille2 = create_bottle("Château Margaux", "Margaux", "Rouge", "Bordeaux", 2016, 1200, "photo.jpg")
 
-		self.assertEqual(bouteilles_triees[0][5], 2015)  # Année
-		self.assertEqual(bouteilles_triees[1][5], 2016)
+    etagere1.ajouter_bouteille(bouteille1)
+    etagere1.ajouter_bouteille(bouteille2)
 
-	def test_ajout_note_commentaire(self):
-		"""Test de l'ajout d'une note et d'un commentaire à une bouteille"""
-		bouteille = Bouteille("Domaine de la Romanée-Conti", "Romanée-Conti", "Rouge", "Bourgogne", 2015, 15000, "photo.jpg")
-		self.etagere1.ajouter_bouteille(bouteille)
+    cursor.execute("SELECT * FROM bouteilles ORDER BY annee")
+    bouteilles_triees = cursor.fetchall()
 
-		bouteilles = Bouteille.get_bouteilles_by_etagere(self.etagere1.etagere_id)
-		bouteille_id = bouteilles[0]['bouteille_id']
+    assert bouteilles_triees[0][5] == 2015  # Année
+    assert bouteilles_triees[1][5] == 2016
 
-		Communaute.ajouter_note_commentaire(self.user1.user_id, bouteille_id, 5, "Excellent vin !")
+def test_ajout_note_commentaire(setup_db):
+    """Test de l'ajout d'une note et d'un commentaire à une bouteille"""
+    db, cursor, user1, _, _, etagere1 = setup_db
+    bouteille = create_bottle("Domaine de la Romanée-Conti", "Romanée-Conti", "Rouge", "Bourgogne", 2015, 15000, "photo.jpg")
+    etagere1.ajouter_bouteille(bouteille)
+    
+    bouteilles = Bouteille.get_bouteilles_by_etagere(etagere1.etagere_id)
+    bouteille_id = bouteilles[0]['bouteille_id']
+    
+    Communaute.ajouter_note_commentaire(user1.user_id, bouteille_id, 5, "Excellent vin !")
+    
+    # Modify cursor to return a dictionary
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM notes_commentaires WHERE bouteille_id = %s", (bouteille_id,))
+    notes_commentaires = cursor.fetchall()
+    
+    assert len(notes_commentaires) == 1
+    assert notes_commentaires[0]['note'] == 5  # Access the 'note' field directly by name
+    assert notes_commentaires[0]['commentaire'] == "Excellent vin !"  # Access 'commentaire' by name
 
-		self.cursor.execute("SELECT * FROM notes_commentaires WHERE bouteille_id = %s", (bouteille_id,))
-		notes_commentaires = self.cursor.fetchall()
+def test_calculer_note_moyenne(setup_db):
+    """Test du calcul de la note moyenne d'une bouteille"""
+    db, cursor, user1, user2, _, etagere1 = setup_db
+    bouteille = create_bottle("Domaine de la Romanée-Conti", "Romanée-Conti", "Rouge", "Bourgogne", 2015, 15000, "photo.jpg")
+    etagere1.ajouter_bouteille(bouteille)
 
-		self.assertEqual(len(notes_commentaires), 1)
-		self.assertEqual(notes_commentaires[0][2], 5)  # Note
-		self.assertEqual(notes_commentaires[0][3], "Excellent vin !")  # Commentaire
+    bouteilles = Bouteille.get_bouteilles_by_etagere(etagere1.etagere_id)
+    bouteille_id = bouteilles[0]['bouteille_id']
 
-	def test_calculer_note_moyenne(self):
-		"""Test du calcul de la note moyenne d'une bouteille"""
-		bouteille = Bouteille("Domaine de la Romanée-Conti", "Romanée-Conti", "Rouge", "Bourgogne", 2015, 15000, "photo.jpg")
-		self.etagere1.ajouter_bouteille(bouteille)
+    Communaute.ajouter_note_commentaire(user1.user_id, bouteille_id, 4, "Très bon vin")
+    Communaute.ajouter_note_commentaire(user2.user_id, bouteille_id, 5, "Excellent vin")
 
-		bouteilles = Bouteille.get_bouteilles_by_etagere(self.etagere1.etagere_id)
-		bouteille_id = bouteilles[0]['bouteille_id']
-
-		Communaute.ajouter_note_commentaire(self.user1.user_id, bouteille_id, 4, "Très bon vin")
-		Communaute.ajouter_note_commentaire(self.user2.user_id, bouteille_id, 5, "Excellent vin")
-
-		moyenne = Communaute.calculer_note_moyenne(bouteille_id)
-		self.assertEqual(moyenne, 4.5)
-
-if __name__ == '__main__':
-	unittest.main()
+    moyenne = Communaute.calculer_note_moyenne(bouteille_id)
+    assert moyenne == 4.5
